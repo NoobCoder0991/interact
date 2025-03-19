9a5b312629de8c245b807ea8a4503698a9e187b3
shafaat Hussain <shafaath508@gmail.com>
2025-03-19 12:44:03 +0530

    fixed a production issue

--- database.js
+++ database.js
@@ -1,13 +1,15 @@
 const { MongoClient, ServerApiVersion, MongoGridFSChunkError, GridFSBucket } = require("mongodb");
 // Replace the placeholder with your Atlas connection string
-const uri = "mongodb://127.0.0.1:27017";
+const local_uri = "mongodb://127.0.0.1:27017";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 require('dotenv').config();
 
 const username = encodeURIComponent(process.env.MONGODB_USER);
 const password = encodeURIComponent(process.env.MONGODB_PASS);
 
-// const uri = `mongodb+srv://${username}:${password}@cluster0.ic3ix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const production_uri = `mongodb+srv://${username}:${password}@cluster0.ic3ix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const uri = process.env.NODE_ENV == 'production' ? production_uri : local_uri;
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
